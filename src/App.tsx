bc47d786cec874f918a576c5857b41daa39bcf76
Rubens Brock <r.brocksilva@yahoo.com.br>
2021-05-24 15:57:32 -0300

    types in another file OK

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,14 @@ import { MovieCard } from './components/MovieCard';
 // import { Content } from './components/Content';
 
 import { api } from './services/api';
+import { GenreResponseProps, MovieProps } from './utils/types';
 
 import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
 
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
